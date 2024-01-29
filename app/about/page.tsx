0127d37d78ccf543ae49db277e04cdf7cd2f3e1c
Devansh Dixit <102344965+DevanshDixitDD@users.noreply.github.com>
2024-01-29 19:32:01 +0530

    First Commit

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+import Courses from '@/components/Courses'
+import Expert from '@/components/Expert'
+import Features from '@/components/Features'
+import Hero from '@/components/Hero'
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <>
+    <Hero/>
+    <Courses/>
+    <Expert/>
+    <Features/>
+    
+    </>
+  )
+}
+
+export default AboutPage
